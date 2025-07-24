181464559abf76e1a0519c3654d477175b482363
Hu Xinhao <417367503@qq.com>
2025-07-24 11:23:55 +0800

    update rethinking

--- src/predictor.py
+++ src/predictor.py
@@ -106,7 +106,7 @@ Example:(Assume door is not included in the given scene map, but you can infer i
 {"imagined_view": ['door', 'shower', 'mirror', 'cloth']}
 ...
 Most importantly! The objects in the imagined_view and action_chain can only be chosen from:'''
-        self.system_prompt += f'''{self.nodespace}\n'''
+        self.system_prompt += f'''{self.nodespace}. And the target object must be subset of the imagined_view.'''
         self.system_prompt += f'''Do **not** include any other explanations or extra text in your output.
 Remeber you must generate enough steps of actions and matched imagined_view to completely express the requirements in the instruction and make sure the agent can gradually approach the goal!! Think carefully of the objects you will see along the way!
 '''
@@ -163,7 +163,7 @@ Remeber you must generate enough steps of actions and matched imagined_view to c
             self.user_prompt += f'''{self.response}\n'''
             self.user_prompt += f'''Now you have successfully reached the first {self.error_action_num-1} target objects in the action chain.\n'''
             self.user_prompt += f'''But in actual situation, the agent can't {self.error_object} at "action_num {self.error_action_num}". The previos predicted view{self.error_imagined_graph} is far different from real observation {current_local_detected_graph}''' # 错误反馈
-            self.user_prompt += f'''Now you need to rethink the action chain based on the current state, error information above and the following real scene map.\n'''
+            self.user_prompt += f'''What should the agent do next? Now you need to rethink next actions and views. Give the action chain based on the current state, error information above and the following real scene map.\n'''
 
         self.user_prompt += "scene map:{"
         self.user_prompt += f'''{self.graph_to_string()} \n'''
@@ -180,7 +180,7 @@ Remeber you must generate enough steps of actions and matched imagined_view to c
             objects_of_time = self.detected_graph.time_to_objects[time_id]
             #print(f"time_id = {time_id}, objects = {objects}")
             string_map += f"{list(objects_of_time)}\n"
-        print(f"object-group {string_map}")
+        # print(f"object-group {string_map}")
         return string_map
 
     def get_llm_response(self):
@@ -238,11 +238,11 @@ Remeber you must generate enough steps of actions and matched imagined_view to c
                         self.imagined_graph_chain.append(data["imagined_view"])
                         self.imagined_graph_chain[-1].append('NORMAL')  # 添加停止动作
                         # imagined_view 的总长度比action_chain少1
-                print("imagined_graph_chain =", self.imagined_graph_chain)
+                # print("decomposed imagined_graph_chain =", self.imagined_graph_chain)
                 # 根据 num_of_order 排序 action
                 self.action_chain.sort(key=lambda x: x[0])
                 self.action_chain[-1]=(self.action_chain[-1][0],self.action_chain[-1][1],"STOP") # 添加停止动作
-                self.imagined_graph_chain[-1].append('STOP')  # 最后一个动作的想象内容为空
+                self.imagined_graph_chain[-1][-1]='STOP'  # 最后一个动作的想象内容为空
 
                 # 输出结果
                 #print("target_chain =", target_chain)
@@ -262,10 +262,10 @@ Remeber you must generate enough steps of actions and matched imagined_view to c
         #print(self.system_prompt)
         #print(self.user_prompt)
         self.response=self.get_llm_response()
-        print(self.response)
+        print("origin response=",self.response)
         self.response_extractor()
-        print("action_chain =", self.action_chain)
-        print("imagined_graph_chain =", self.imagined_graph_chain)
+        print("extracted action_chain =", self.action_chain)
+        print("extracted imagined_graph_chain =", self.imagined_graph_chain)
         num_steps = len(self.imagined_graph_chain)
         max_time_id = max(self.detected_graph.get_time_values())
         for i in range(num_steps):
@@ -311,12 +311,22 @@ if __name__ == "__main__":
     print("action_chain =", predictor.action_chain)
     error_point= predictor.action_chain[-2]  # 假设倒数第二个动作是错误的
     print("error_point =", error_point)
+    print("*"*50)
+    predictor.set_current_position(error_point[1])  # 更新当前位置为错误动作的目标对象
+    renewed_graph = OptimizedTimeObjectGraph()
+    renewed_graph.add_recognition(0, ['chair', 'sofa', 'door', 'window', 'table'])
+    renewed_graph.add_recognition(1, ['sofa', 'refrigerator', 'door', 'table', 'vase'])
+    renewed_graph.add_recognition(2, ['door', 'tv', 'sink'])
+    renewed_graph.add_recognition(3, ['sink', 'oven', 'microwave', 'refrigerator'])  # 假设在错误动作后看到了这些物体
+    predictor.load_detected_graph(renewed_graph)  # 更新探测图
+    print("rethinking...")
     predictor.rethinking(error_action=error_point)
 
     ##TODO: goal是子集的命令调试验证
+    ### RESULT：很不稳定，经常不在子集中；手动加入？
+    ##TODO: 预测效果验证
+    ### RESULT：有时预测的步骤多于所需，给出多余的步骤导航阶段数量————估计需要动态“stop”（每一步都做与终点想象图的相似度计算？开销大吗）
     ##TODO: skill暂时没有用到导航中
-    ##TODO: self.current_position没有与探测/任务初始化/current_step保持同步更新，这个参数需要考虑“实时更新”或删除
-    ##      rethinking时重新设置self.current_position感觉更好一些！！！！！！important!!!!
+    ##TODO: self.current_position没有与探测/任务初始化/current_step保持同步更新，完整程序中必须修改后才可以开始预测或者rethink!
     ## optional:
-    ## memory module
-    ## 逐步提问CoT
+    ## 逐步提问CoT————现在感觉是有必要的：根据指令要预测几步？->终点物体 ->终点处场景 ->过程中场景
